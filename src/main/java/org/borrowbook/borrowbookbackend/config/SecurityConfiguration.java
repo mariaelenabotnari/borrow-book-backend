7de5cd24f769e2a6d742d65ba64334275dcec42f
MaxNoragami <maxcraftmd@gmail.com>
2025-09-10 09:59:11 +0300

    chore: get rid of an allowed origin for CORS

--- src/main/java/org/borrowbook/borrowbookbackend/config/SecurityConfiguration.java
+++ src/main/java/org/borrowbook/borrowbookbackend/config/SecurityConfiguration.java
@@ -95,8 +95,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "https://accounts.google.com"
+                "http://localhost:3000"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
