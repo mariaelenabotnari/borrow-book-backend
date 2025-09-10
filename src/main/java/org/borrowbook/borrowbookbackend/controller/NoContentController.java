09a470dedbc257ba271d3178d420e7be8f2444ca
mariaelenabotnari <152028668+mariaelenabotnari@users.noreply.github.com>
2025-09-10 12:01:39 +0300

    fix: last fix

--- src/main/java/org/borrowbook/borrowbookbackend/controller/NoContentController.java
+++ src/main/java/org/borrowbook/borrowbookbackend/controller/NoContentController.java
@@ -1,10 +1,11 @@
 package org.borrowbook.borrowbookbackend.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class NoContentController {
     @GetMapping("/favicon.ico")
     @ResponseStatus(HttpStatus.NO_CONTENT)
