f74cb830fae8f30ed429677cc9892b860fd762cb
MaxNoragami <maxcraftmd@gmail.com>
2025-09-17 10:28:24 +0300

    fix: make sure all pagination stats are included in response

--- src/main/java/org/borrowbook/borrowbookbackend/model/dto/PaginatedResultDTO.java
+++ src/main/java/org/borrowbook/borrowbookbackend/model/dto/PaginatedResultDTO.java
@@ -10,6 +10,8 @@ public class PaginatedResultDTO<T> {
     private final int pageSize;
     private final long totalCount;
     private final int totalPages;
+    private final boolean hasNextPage;
+    private final boolean hasPreviousPage;
     private final List<T> items;
 
     public PaginatedResultDTO(int pageIndex, int pageSize, long totalCount, List<T> items) {
@@ -17,6 +19,8 @@ public class PaginatedResultDTO<T> {
         this.pageSize = pageSize;
         this.totalCount = totalCount;
         this.totalPages = (int) Math.ceil((double) totalCount / pageSize);
+        this.hasNextPage = pageIndex < totalPages;
+        this.hasPreviousPage = pageIndex > 1;
         this.items = items;
     }
 
