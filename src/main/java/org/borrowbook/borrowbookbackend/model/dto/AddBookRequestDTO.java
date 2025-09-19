54816994cf470c22b37ffb8e63020e8c82802361
MaxNoragami <maxcraftmd@gmail.com>
2025-09-19 10:37:01 +0300

    fix: add missing validation checks to request DTOs

--- src/main/java/org/borrowbook/borrowbookbackend/model/dto/AddBookRequestDTO.java
+++ src/main/java/org/borrowbook/borrowbookbackend/model/dto/AddBookRequestDTO.java
@@ -1,10 +1,13 @@
 package org.borrowbook.borrowbookbackend.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.borrowbook.borrowbookbackend.model.enums.BookStatus;
 
 @Data
 public class AddBookRequestDTO {
+    @NotBlank(message = "Google Book ID is required")
     private String googleBookId;
+    @NotBlank(message = "Status is required")
     private BookStatus status;
 }
