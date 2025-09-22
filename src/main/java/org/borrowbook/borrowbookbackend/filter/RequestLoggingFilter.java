383e5d256aa2cd86971954b86d648a928a2fa186
MaxNoragami <maxcraftmd@gmail.com>
2025-09-22 18:57:49 +0300

    feat: add a filter for logging the calls to controllers

---
+++ src/main/java/org/borrowbook/borrowbookbackend/filter/RequestLoggingFilter.java
@@ -0,0 +1,39 @@
+package org.borrowbook.borrowbookbackend.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        String remoteAddr = getClientIpAddress(request);
+
+        log.info("{} {} called from IP: {}", method, uri, remoteAddr);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getClientIpAddress(HttpServletRequest request) {
+        String xForwardedForHeader = request.getHeader("X-Forwarded-For");
+        if (xForwardedForHeader == null) {
+            return request.getRemoteAddr();
+        } else {
+            return xForwardedForHeader.split(",")[0];
+        }
+    }
+}
