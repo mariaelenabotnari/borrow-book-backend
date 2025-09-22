818e4a9df802c110877071da0a0d98316c7d6e1e
MaxNoragami <maxcraftmd@gmail.com>
2025-09-22 15:24:11 +0300

    feat: add scheduled job to self ping every 13 min so deployed service won't go down

--- src/main/java/org/borrowbook/borrowbookbackend/BorrowBookBackendApplication.java
+++ src/main/java/org/borrowbook/borrowbookbackend/BorrowBookBackendApplication.java
@@ -2,8 +2,10 @@ package org.borrowbook.borrowbookbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BorrowBookBackendApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/borrowbook/borrowbookbackend/service/scheduler/KeepAliveService.java
@@ -0,0 +1,29 @@
+package org.borrowbook.borrowbookbackend.service.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KeepAliveService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${app.base-url}")
+    private String baseUrl;
+
+    @Scheduled(fixedRate = 780000)
+    public void keepAlive() {
+        try {
+            restTemplate.getForObject(baseUrl + "/api/v1/auth/get", Void.class);
+            log.info("Keep-alive ping to /get successful");
+        } catch (Exception e) {
+            log.warn("Keep-alive ping to /get failed: {}", e.getMessage());
+        }
+    }
+}
